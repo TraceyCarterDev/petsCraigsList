fd7f1d99d5dad273ba790a27f14e590e4ba382dc
Tracey Carter <traceycarterdbc@gmail.com>
2015-10-28 15:34:56 -0700

    add app controller and routesrb changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def welcome
+    render text: "Welcome to Petslist.org, a Craig's list for pets and pet needs!!"
+  end
 end
